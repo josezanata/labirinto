709a0fee816db54a6ea3f6deefdeb1198598bb4c
josezanata <jose.krais@escola.pr.gov.br>
2026-03-23 09:09:50 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+let matriz =[
+    [22, 45, 78, 89],
+    [4, 5, 6, 86],
+    [7, 8, 9, 76]
+]
+
+for(let i=0; i<matriz.length;i++){
+    for(let j=0; j<matriz[i].length; j++){
+        console.log(matriz[i][j]);
+
+    }
+    console.log(matriz[1][3]);
+    
+}
